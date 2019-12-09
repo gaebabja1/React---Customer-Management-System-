37f06d92c483a3b3d666a714f177606069c4bb9b
gaebabja <hippo420@naver.com>
2019-12-09 22:26:01 +0900

    구조화 및 map활용 데이터출력

--- src/App.js
+++ src/App.js
@@ -2,22 +2,51 @@ import React, { Component } from 'react';
 import Student from './components/Student'
 import './App.css';
 
-const student ={
+const student =[{
+  'id':'1',
   'name':'개밥자',
+  'image':'https://placeimg.com/64/64/1',
   'student_number': '201915243',
-  'major':'컴퓨터공학',
+  'major':'컴퓨터공학과',
   'gpa':'4.09'
+},
+{
+  'id':'2',
+  'name':'김오즈',
+  'image':'https://placeimg.com/64/64/2',
+  'student_number': '201948651',
+  'major':'산업공학과',
+  'gpa':'3.09'
+},
+{
+  'id':'3',
+  'name':'김첵스',
+  'image':'https://placeimg.com/64/64/3',
+  'student_number': '201995864',
+  'major':'경영학과',
+  'gpa':'4.00'
 }
+
+]
 
 class App extends Component{
   render(){
     return(
-     <Student
-       name={student.name}
-       student_number={student.student_number}
-       major={student.major}
-       gpa={student.gpa}
-      />
+     <div>
+       {
+         student.map(c=>{
+           return(<Student
+            key = {c.id}
+            id = {c.id}
+            image = {c.image}
+            name = {c.name}
+            student_number = {c.student_number}
+            major = {c.major}
+            gpa = {c.gpa}
+           />)
+         })
+       }
+     </div>
     );
   }
 }
